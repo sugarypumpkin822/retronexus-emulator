3f10f6a1ab3a50fd09e9dded335b833d9438b6a5
ryan allemand <ryanallemand69@gmail.com>
2025-05-21 18:28:49 -0700

    Add files via upload

---
+++ ConsoleType.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+enum class ConsoleType {
+    UNKNOWN,
+    // Nintendo
+    NES,
+    SNES,
+    N64,
+    GAMECUBE,
+    GAMEBOY,
+    GAMEBOY_COLOR,
+    GAMEBOY_ADVANCE,
+    DS,
+    
+    // Sony
+    PS1,
+    PS2,
+    PSP,
+    PSV,
+    
+    // Sega
+    MASTER_SYSTEM,
+    GENESIS,
+    SATURN,
+    DREAMCAST,
+    GAME_GEAR,
+    
+    // Atari
+    ATARI_2600,
+    ATARI_5200,
+    ATARI_7800,
+    
+    // Other
+    NEO_GEO,
+    NEO_GEO_POCKET,
+    WONDERSWAN,
+    TURBOGRAFX_16
+}; 
